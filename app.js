9a6745981183348451cd850972f671b0aa07c4b5
tom-smith-okta <tom.smith@okta.com>
2018-11-01 21:09:49 -0400

    initial commit

--- app.js
+++ app.js
@@ -20,3 +20,18 @@ app.listen(port, function () {
 })
 
 //////////////////////////////////////////////////
+
+// HOME PAGE
+app.get('/', function (req, res) {
+	fs.readFile('html/index.html', (err, data) => {
+		if (err) {
+			console.log("error reading the index.html file")
+		}
+
+		var page = data.toString()
+
+		page = page.replace(/{{REDIRECT_URI}}/g, process.env.REDIRECT_URI)
+
+		res.send(page)
+	})
+})
